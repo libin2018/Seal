13e9816d0daf78601455a8a1b48553627b259eb2
libin <libin>
2020-01-21 12:47:21 +0800

    '20.01.21'

---
+++ master/Seal/frmMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Seal
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picSeal = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.numPix = new System.Windows.Forms.NumericUpDown();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picSeal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picSeal
+            // 
+            this.picSeal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSeal.Location = new System.Drawing.Point(12, 39);
+            this.picSeal.Name = "picSeal";
+            this.picSeal.Size = new System.Drawing.Size(600, 600);
+            this.picSeal.TabIndex = 1;
+            this.picSeal.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // numPix
+            // 
+            this.numPix.Location = new System.Drawing.Point(12, 12);
+            this.numPix.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numPix.Name = "numPix";
+            this.numPix.Size = new System.Drawing.Size(75, 21);
+            this.numPix.TabIndex = 5;
+            this.numPix.Value = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.numPix.KeyUp += new System.Windows.Forms.KeyEventHandler(this.numPix_KeyUp);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(94, 11);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 6;
+            this.btnSelect.Text = "选择图片";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(174, 11);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "保存图片";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 650);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.numPix);
+            this.Controls.Add(this.picSeal);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "公章透明处理，尺寸600,600";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResizeBegin += new System.EventHandler(this.frmMain_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.frmMain_ResizeEnd);
+            this.Move += new System.EventHandler(this.frmMain_Move);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.picSeal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPix)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picSeal;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.NumericUpDown numPix;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ master/Seal/frmMain.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seal
+{
+    public partial class frmMain : Form
+    {
+        public int pix = 600;
+
+        private Color tr_color = Color.Transparent;
+        private bool b_start = false;
+        bool[] b_visible = null;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            BeginSet();
+            Setting();
+            EndSet();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "选择公章";
+            openFileDialog1.Filter = "Image Files(*.PNG;*.JPG;)|*.PNG;*.JPG|All files(*.*)|*.*";
+            openFileDialog1.RestoreDirectory = false;
+            openFileDialog1.ShowDialog();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "保存公章";
+            saveFileDialog1.Filter = "png图片(*.png)|*.png";
+            saveFileDialog1.RestoreDirectory = false;
+            saveFileDialog1.FileName = openFileDialog1.FileName.ToLower().Replace(".jpg", ".png");
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string fileName = openFileDialog1.FileName;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return;
+            }
+
+            var image = Image.FromFile(fileName);
+
+            picSeal.Image = image;
+
+            ConvertImage(image);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            picSeal.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void numPix_KeyUp(object sender, KeyEventArgs e)
+        {
+            InitForm();
+        }
+
+        public void InitForm()
+        {
+            pix = Convert.ToInt32(numPix.Value);
+            this.Text = string.Format("公章透明处理，尺寸({0},{0})", pix);
+            this.Width = pix + 40;
+            this.Height = pix + 90;
+            this.picSeal.Width = pix;
+            this.picSeal.Height = pix;
+
+            SetBackgroundImageTransparent();
+        }
+
+        public void ConvertImage(Image image)
+        {
+            Task t = new Task(() =>
+            {
+                Thread.Sleep(2000);
+
+                image = KnockOutGzf(image);
+
+                image = ChangeImgSize(image, pix, pix);
+
+                picSeal.Image = image;
+            });
+            t.Start();
+        }
+
+        #region 图片加工
+
+        public static Bitmap KnockOutGzf(Image image)
+        {
+            var bitmapProxy = new System.Drawing.Bitmap(image);
+            image.Dispose();
+            for (int i = 0; i < bitmapProxy.Width; i++)
+            {
+                for (int j = 0; j < bitmapProxy.Height; j++)
+                {
+                    System.Drawing.Color c = bitmapProxy.GetPixel(i, j);
+                    if (!(c.R < 240 || c.G < 240 || c.B < 240))
+                    {
+                        bitmapProxy.SetPixel(i, j, System.Drawing.Color.Transparent);
+                    }
+                }
+            }
+            return bitmapProxy;
+        }
+
+        public static Bitmap ChangeImgSize(Image bit, int width, int height)
+        {
+            var newBitmap = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(newBitmap);
+            g.Clear(Color.Transparent);
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            g.DrawImage(bit, new Rectangle(0, 0, newBitmap.Width, newBitmap.Height), 0, 0, bit.Width, bit.Height, GraphicsUnit.Pixel);
+            g.Dispose();
+            return newBitmap;
+        }
+
+        #endregion
+
+        #region 设置透明背景
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            Setting();
+        }
+
+        private void frmMain_ResizeBegin(object sender, EventArgs e)
+        {
+            BeginSet();
+        }
+
+        private void frmMain_ResizeEnd(object sender, EventArgs e)
+        {
+            EndSet();
+        }
+
+        private void frmMain_Move(object sender, EventArgs e)
+        {
+            Setting();
+        }
+
+        private void SetBackgroundImageTransparent()
+        {
+            Point pt = this.PointToScreen(this.picSeal.Location);
+            var b = new Bitmap(this.picSeal.Width, this.picSeal.Height);
+            using (Graphics g = Graphics.FromImage(b))
+            {
+                g.CopyFromScreen(pt, new Point(), new Size(this.picSeal.Width, this.picSeal.Height));
+            }
+
+            this.picSeal.BackgroundImage = b;
+        }
+
+        private void BeginSet()
+        {
+            tr_color = this.TransparencyKey;
+            b_start = true;
+        }
+
+        private void Setting()
+        {
+            if (b_start)
+            {
+                b_visible = new bool[Controls.Count];
+                for (int i = 0; i < Controls.Count; i++)
+                {
+                    b_visible[i] = Controls[i].Visible;
+                    Controls[i].Visible = false;
+                }
+                BackgroundImage = null;
+                BackColor = Color.White;
+                b_start = false;
+                this.TransparencyKey = Color.White;
+            }
+        }
+
+        private void EndSet()
+        {
+            SetBackgroundImageTransparent();
+            this.TransparencyKey = tr_color;
+            for (int i = 0; i < Controls.Count; i++)
+            {
+                Controls[i].Visible = b_visible[i];
+            }
+            b_start = false;
+        }
+
+        #endregion
+    }
+}
